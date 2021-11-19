cd5f4f875d5763c25206ba9708ff32dd741e237d
bjhuangr <bjhuangr@cn.ibm.com>
2021-11-19 10:57:07 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import platform
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World from %s!\n" % platform.uname().machine
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
