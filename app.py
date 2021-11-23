3940fae27d6d2d80da5042f2a1e77060d224b342
bjhuangr <bjhuangr@cn.ibm.com>
2021-11-23 10:16:24 +0800

    upper world

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello world, from %s!\n" % platform.uname().machine
+    return "Hello WORLD, from %s!\n" % platform.uname().machine
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
